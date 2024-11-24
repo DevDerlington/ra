1a5146b2dc2f1f5e758d41c8c370ecc3f04c327e
unknown <anukaniranlowo22@gmail.com>
2024-11-24 22:08:32 +0100

    Random Emojies

---
+++ app.js
@@ -0,0 +1,38 @@
+const btn = document.getElementById("emoji");
+const emojis = [
+"🤔",
+"💫",
+"🎶",
+"😘",
+"💕",
+"😂",
+"😎",
+"🤷‍♂️",
+"🤳",
+"👍",
+"🙌",
+"🐱‍👤",
+"🐱",
+"🐱‍💻",
+"🤣",
+"💖",
+"😁",
+"😢",
+"👏",
+"💨",
+"🌹",
+"✔",
+"🤢",
+"👀",
+"✨",
+"💋",
+];
+
+
+btn.addEventListener("mouseover", ()=>{
+    btn.innerHTML= emojis[Math.floor(Math.random() * emojis.length)];
+});
+
+
+
+
